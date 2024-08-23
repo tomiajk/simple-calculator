1769e660eec9e37188925c5a3bbb6ccd3e489cc6
tomiajk <tomiajaka1@gmail.com>
2024-08-23 19:34:23 +0100

    cal

--- scripts/simplecalculator.js
+++ scripts/simplecalculator.js
@@ -1,11 +1,78 @@
 const mainDisplayBox = document.querySelector('.display-box-main');
 const numberButtons = document.querySelectorAll('.number-button');
+const deleteButton = document.querySelector('.js-delete-button');
+const clearButton = document.querySelector('.js-clear-button');
+const answerBox = document.querySelector('.js-answer-box');
+const addButton = document.querySelector('.js-add-button');
+const topDisplay = document.querySelector('.js-top-display');
+const equalToButton =document.querySelector('.js-equal-to-button');
 
-const oneButton = document.querySelector('.oneButton');
+let lastNumber = 0;
+let lastSign;
+let topDisplayContent = '';
+
+mainDisplayBox.innerHTML = ''
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    number = button.innerHTML;
-    mainDisplayBox.innerHTML += number
+    char = button.innerHTML;
+    mainDisplayBox.innerHTML += char
   });
 })
+
+deleteButton.addEventListener("click", () => {
+  initial =  mainDisplayBox.innerHTML.split('');
+  sliced = initial.slice(0, -1);
+  mainDisplayBox.innerHTML = sliced.join('')
+})
+
+clearButton.addEventListener("click", () => {
+  mainDisplayBox.innerHTML = '';
+  answerBox.innerHTML = ''
+  topDisplay.innerHTML = ''
+  clear();
+})
+
+addButton.addEventListener("click", () => {
+  number = getNumberFromDisplay();
+  lastNumber += number;
+  topDisplayContent += `${number} + `;
+  lastSign = 'add';
+  showSmallDisplay();
+  showTopDisplay();
+})
+
+equalToButton.addEventListener("click", () => {
+  number = getNumberFromDisplay();
+  if (number) {
+    switch (lastSign) {
+      case ('add'):
+        lastNumber += number
+        break
+    }
+  
+    topDisplayContent += `${number}`;
+    showSmallDisplay();
+    showTopDisplay();
+    clear();
+  }
+});
+
+const getNumberFromDisplay = () => {
+  number = Number(mainDisplayBox.innerHTML);
+  mainDisplayBox.innerHTML = '';
+  return number
+};
+
+const showSmallDisplay = () => {
+  answerBox.innerHTML = `<div class="display-box-small js-answer-box">= ${lastNumber}</div>`
+};
+
+const showTopDisplay = () => {
+  topDisplay.innerHTML = topDisplayContent;
+};
+
+const clear = () => {
+  lastNumber = 0;
+  topDisplayContent = '';
+}
